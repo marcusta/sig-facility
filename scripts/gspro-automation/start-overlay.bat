b50d724fb15522eba9b5d735c16a2465cb5c9dd1
Marcus Andersson <marcus.andersson1975@axis.com>
2026-01-31 23:58:26 +0100

    Update adbanced visual data overlay position

--- scripts/gspro-automation/start-overlay.bat
+++ scripts/gspro-automation/start-overlay.bat
@@ -1,2 +1,2 @@
 cd "C:\Program Files (x86)\OnTopReplica\OnTopReplica"
-start OnTopReplica.exe --windowTitle=Advanced --width=430 --position=1500,850  --opacity=200
+start OnTopReplica.exe --windowTitle=Advanced --width=444 --position=9,593 --opacity=200
